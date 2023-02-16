2d3ca0f9968b9f2c5819e15c52cdfc8862c49858
Theodora <sc20tmv@leeds.ac.uk>
2023-02-16 19:28:42 +0000

    tree test

--- Turtle/Turtle.cpp
+++ Turtle/Turtle.cpp
@@ -5,32 +5,18 @@
 #include "Turtle.h"
 
 
+
 void Turtle::computeFinalWorldM() {
+    treeTest();
+
+    finalWorldMatrices = {
+            worldMatrices.size(),
+            glm::mat4(1.0f)
+    };
+
     for (unsigned int i = 1; i < worldMatrices.size(); i++) {
         finalWorldMatrices[i] = finalWorldMatrices[parentVectors[i]] * worldMatrices[i];
     }
-
-    for (int i = 3; i < 132; i++) //leafs
-    {
-        worldMatrices.emplace_back(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f))) * glm::rotate(glm::mat4(1.0f), (kPi_ / -6.f), glm::vec3(0.0f, 0.0f, 1.0f));
-        parentVectors.emplace_back(i);
-        worldMatrices.emplace_back(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f))) * glm::rotate(glm::mat4(1.0f), (kPi_ / 6.f), glm::vec3(0.0f, 0.0f, 1.0f));
-        parentVectors.emplace_back(i);
-
-        worldMatrices.emplace_back(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f))) * glm::rotate(glm::mat4(1.0f), (kPi_ / -6.f), glm::vec3(1.0f, 0.0f, 0.0f));
-        parentVectors.emplace_back(i);
-        worldMatrices.emplace_back(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f))) * glm::rotate(glm::mat4(1.0f), (kPi_ / 6.f), glm::vec3(1.0f, 0.0f, 0.0f));
-        parentVectors.emplace_back(i);
-
-        worldMatrices.emplace_back(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f))) * glm::rotate(glm::mat4(1.0f), (kPi_ / -6.f), glm::vec3(0.0f, 1.0f, 0.0f));
-        parentVectors.emplace_back(i);
-        worldMatrices.emplace_back(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f))) * glm::rotate(glm::mat4(1.0f), (kPi_ / 6.f), glm::vec3(0.0f, 1.0f, 0.0f));
-        parentVectors.emplace_back(i);
-    }
-}
-
-std::vector<glm::mat4> Turtle::returnFinalWorldM() {
-    return  finalWorldMatrices;
 }
 
 void Turtle::draw(
@@ -45,5 +31,46 @@ void Turtle::draw(
         shader->setMat4("model", finalWorldMatrices[i]);
         glDrawArrays(GL_TRIANGLES, 0, shape->getVertexCount() * 3);
 
+    }
+}
+
+void Turtle::treeTest()
+{
+    std::vector<glm::mat4> branches = {
+            glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f)),
+            glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f)),
+            glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f)) * glm::rotate(glm::mat4(1.0f), (kPi_ / 3.f), glm::vec3(0.0f, 0.0f, 1.0f)) * glm::scale(glm::mat4(1.0f), glm::vec3(0.75f, 0.75f, 0.75f)),
+            glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f)) * glm::rotate(glm::mat4(1.0f), (-kPi_ / 3.f), glm::vec3(0.0f, 0.0f, 1.0f)) * glm::scale(glm::mat4(1.0f), glm::vec3(0.75f, 0.75f, 0.75f)),
+            glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 0.f, 0.f)) * glm::rotate(glm::mat4(1.0f), (kPi_ / -3.f), glm::vec3(1.0f, 0.0f, 0.0f)),
+            glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 0.f, 0.f)) * glm::rotate(glm::mat4(1.0f), (kPi_ / 3.f), glm::vec3(1.0f, 0.0f, 0.0f))
+    };
+    std::copy(begin(branches), end(branches), std::back_inserter(worldMatrices));
+
+    std::vector<int> branchesParents = {
+            1,
+            2,
+            2,
+            2,
+            3,
+            3
+    };
+    std::copy(begin(branchesParents), end(branchesParents), std::back_inserter(parentVectors));
+
+    for (int i = 3; i < 132; i++)
+    {
+        worldMatrices.emplace_back(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f)) * glm::rotate(glm::mat4(1.0f), (kPi_ / -6.f), glm::vec3(0.0f, 0.0f, 1.0f)));
+        parentVectors.emplace_back(i);
+        worldMatrices.emplace_back(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f)) * glm::rotate(glm::mat4(1.0f), (kPi_ / 6.f), glm::vec3(0.0f, 0.0f, 1.0f)));
+        parentVectors.emplace_back(i);
+
+        worldMatrices.emplace_back(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f)) * glm::rotate(glm::mat4(1.0f), (kPi_ / -6.f), glm::vec3(1.0f, 0.0f, 0.0f)));
+        parentVectors.emplace_back(i);
+        worldMatrices.emplace_back(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f)) * glm::rotate(glm::mat4(1.0f), (kPi_ / 6.f), glm::vec3(1.0f, 0.0f, 0.0f)));
+        parentVectors.emplace_back(i);
+
+        worldMatrices.emplace_back(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f)) * glm::rotate(glm::mat4(1.0f), (kPi_ / -6.f), glm::vec3(0.0f, 1.0f, 0.0f)));
+        parentVectors.emplace_back(i);
+        worldMatrices.emplace_back(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f)) * glm::rotate(glm::mat4(1.0f), (kPi_ / 6.f), glm::vec3(0.0f, 1.0f, 0.0f)));
+        parentVectors.emplace_back(i);
     }
 }
--- Turtle/Turtle.h
+++ Turtle/Turtle.h
@@ -16,11 +16,10 @@
 
 class Turtle {
 public:
+    void treeTest();
     void computeFinalWorldM();
-    std::vector<glm::mat4> returnFinalWorldM();
     void draw(
             const std::shared_ptr<Shader> &shader,
-//            const T &shape
             const std::shared_ptr<Shape> &shape
             );
 
@@ -28,29 +27,15 @@ private:
     std::vector<glm::mat4> worldMatrices {
         glm::mat4(1.0f),
         glm::translate(glm::mat4(1.0f), glm::vec3( 0.f, 0.f, 0.f)),
-        glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f)),
-        glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f)),
-        glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f)) * glm::rotate(glm::mat4(1.0f), (kPi_ / 3.f), glm::vec3(0.0f, 0.0f, 1.0f)) * glm::scale(glm::mat4(1.0f), glm::vec3(0.75f, 0.75f, 0.75f)),
-        glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 4.f, 0.f)) * glm::rotate(glm::mat4(1.0f), (-kPi_ / 3.f), glm::vec3(0.0f, 0.0f, 1.0f)) * glm::scale(glm::mat4(1.0f), glm::vec3(0.75f, 0.75f, 0.75f)),
-        glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 0.f, 0.f)) * glm::rotate(glm::mat4(1.0f), (kPi_ / -3.f), glm::vec3(1.0f, 0.0f, 0.0f)),
-        glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 0.f, 0.f)) * glm::rotate(glm::mat4(1.0f), (kPi_ / 3.f), glm::vec3(1.0f, 0.0f, 0.0f))
     };
 
     std::vector<int> parentVectors {
             0,
-            0,
-            1,
-            2,
-            2,
-            2,
-            3,
-            3,
+            0
     };
 
-    std::vector<glm::mat4> finalWorldMatrices {
-            worldMatrices.size(),
-            glm::mat4(1.0f)
-    };
+    std::vector<glm::mat4> finalWorldMatrices;
+
     std::shared_ptr<Shader>                         m_shader;
     std::shared_ptr<Cylinder>                       m_cylinder;
     std::shared_ptr<Cone>                           m_cone;
